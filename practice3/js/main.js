c195c3320653b60a7c09cf25a4939953d1842b0b
Talgorhythm <dia.99000@gmail.com>
2018-10-14 07:51:51 +0000

    create spa dir

--- practice3/js/main.js
+++ practice3/js/main.js
@@ -1,3 +1,13 @@
+/* グローバルフィルタ */
+Vue.filter('numberFormat2', function(value){
+  return value.toFixed(2)
+})
+Vue.filter('toUSD', function(jpy){
+  return jpy / 100
+})
+Vue.filter('readMore', function(text, length, suffix){
+  return text.substring(0, length) + suffix
+})
 /* ビューインスタンスの作成 */
 var app = new Vue({
 	//options
@@ -7,14 +17,16 @@ var app = new Vue({
     message2: 'Hello, <i>Vue</i>.js!',
     number: 100,
     ok: true,
-    price: 29800,
+    price: 2000000,
+    jpyPrice: 244444,
+    text: 'Pushed the fader, gifted animator One for the now, and eleven for the later Never made it up to Minnesota',
   },
   methods: {
     clickHandler: function(event){
       // 文字列を反転させる
       this.message = this.message.split('').reverse().join('')
-    }
-  }
+    },
+  },
   //カンマ区切りのフィルタを作成
   filters: {
     numberFormat: function(value){
---
+++ spa/js/spa.shell.js
@@ -0,0 +1,2 @@
+//シェル名前空間spa.shellを提供する。
+//css/spa.shell.cssに対応
